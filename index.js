8745fe90465c8fe520c8b460f273600eba0602da
Ranbo-20171103200 <43673449+Ranbo-20171103200@users.noreply.github.com>
2019-06-18 23:34:37 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+Page({
+  data: {
+    datalist: [{
+        title: "x",
+        price:"150",
+      pic: "https://www.baidu.com/img/bd_logo1.png"
+      },
+      {
+        title: "x",
+        price:"100",
+        pic: "https://www.baidu.com/img/bd_logo1.png"
+      },
+      {
+        title: "x",
+        price:"60",
+        pic: "https://www.baidu.com/img/bd_logo1.png"
+      },
+      {
+        title: "x",
+        price:"140",
+        pic: "https://www.baidu.com/img/bd_logo1.png"
+      }
+    ]
+  },
+})
